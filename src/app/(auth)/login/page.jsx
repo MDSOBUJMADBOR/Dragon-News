2b6db6ad0b00f103618aae1ba5bdf198b79c5659
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-04-26 21:52:04 +0600

    login page added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,57 @@
+'use client'
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
-          return (
-                    <div>
-     Login                         
-                    </div>
-          );
+  const {
+    register,
+    handleSubmit,
+    watch,
+     formState: { errors }
+  } = useForm()
+  console.log(errors);
+
+const handleLoginFunc = (data) => {
+// e.preventDefault();
+// const email = e.target.email.value;
+// const password = e.target.password.value;
+// console.log(email,password);
+}
+console.log( watch('email') );
+console.log( watch('password'));
+
+
+
+  return (
+    <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
+      <div className='p-4 rounded-xl bg-white'>
+<h2 className='font-bold text-3xl text-center mb-6'>Login Your Account</h2>
+
+
+<form className='space-y-4' onSubmit={handleSubmit(handleLoginFunc)}>
+
+<fieldset className="fieldset">
+  <legend className="fieldset-legend">Email address</legend>
+  <input type="email" className="input" placeholder="Enter your email address" {...register("email", { required: 'Email field is required ' })} />
+  {errors.email && <p className='text-red-500 '>{errors.email.message} </p>}
+
+</fieldset>
+<fieldset className="fieldset">
+  <legend className="fieldset-legend">Password</legend>
+  <input type="password" className="input" placeholder="Enter your password" {...register("password", { required: 'Password field is required ' })} />
+  {errors.password && <p className='text-red-500 '>{errors.password.message} </p>}
+
+</fieldset>
+<button className='btn w-full bg-black text-white'>Login</button>
+
+</form>
+<p className='mt-4 '> Don't have an Account? <Link href={'/register'} className='text-blue-500'>Register</Link></p>
+
+      </div>
+
+    </div>
+  );
 };
 
 export default LoginPage;
