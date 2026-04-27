f526aa0506bf8e6e21d06d86dca3b239f371870b
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-04-27 18:24:13 +0600

    proxy

---
+++ src/proxy.js
@@ -0,0 +1,30 @@
+
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
+ 
+export async function proxy(request) {
+          // console.log(request,'re');
+
+ const session = await auth.api.getSession({
+        headers: await headers()
+    })
+console.log(session,'session');
+
+
+
+// const islogingin = false;
+
+if(session){
+          return NextResponse.next();
+}
+
+// return NextResponse.next();
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+ 
+
+ 
+export const config = {
+  matcher: ['/career','/news/:path*']
+}
